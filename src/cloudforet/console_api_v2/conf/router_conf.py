c561b0263919a18598f2f9b7e1626c3002993ec6
Minho Kim <mino@megazone.com>
2022-12-13 15:59:28 +0900

    feat: add Cost Analysis apis
    Signed-off-by: Minho Kim <mino@megazone.com>

--- src/cloudforet/console_api_v2/conf/router_conf.py
+++ src/cloudforet/console_api_v2/conf/router_conf.py
@@ -13,5 +13,70 @@ ROUTER = [
         'path': 'cloudforet.console_api_v2.interface.rest.dashboard.custom_widget:router',
         'prefix': '/dashboard/custom-widget',
         'tags': ['dashboard > custom-widget'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.data_source:router',
+        'prefix': '/cost-analysis/data-source',
+        'tags': ['cost-analysis > data-source'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.schedule:router',
+        'prefix': '/cost-analysis/schedule',
+        'tags': ['cost-analysis > schedule'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.exchange_rate:router',
+        'prefix': '/cost-analysis/exchange-rate',
+        'tags': ['cost-analysis > exchange-rate'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.cost:router',
+        'prefix': '/cost-analysis/cost',
+        'tags': ['cost-analysis > cost'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.data_source_rule:router',
+        'prefix': '/cost-analysis/data-source-rule',
+        'tags': ['cost-analysis > data-source-rule'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.budget:router',
+        'prefix': '/cost-analysis/budget',
+        'tags': ['cost-analysis > budget'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.budget_usage:router',
+        'prefix': '/cost-analysis/budget-usage',
+        'tags': ['cost-analysis > budget-usage'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.public_dashboard:router',
+        'prefix': '/cost-analysis/public-dashboard',
+        'tags': ['cost-analysis > public-dashboard'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.user_dashboard:router',
+        'prefix': '/cost-analysis/user-dashboard',
+        'tags': ['cost-analysis > user-dashboard'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.cost_query_set:router',
+        'prefix': '/cost-analysis/cost-query-set',
+        'tags': ['cost-analysis > cost-query-set'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.custom_widget:router',
+        'prefix': '/cost-analysis/custom-widget',
+        'tags': ['cost-analysis > custom-widget'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.job:router',
+        'prefix': '/cost-analysis/job',
+        'tags': ['cost-analysis > job'],
+    },
+    {
+        'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.job_task:router',
+        'prefix': '/cost-analysis/job-task',
+        'tags': ['cost-analysis > job-task'],
     }
 ]
