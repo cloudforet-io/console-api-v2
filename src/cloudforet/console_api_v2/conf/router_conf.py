aa8e46a1265564536005cd42f242ebe54d420c89
Minho Kim <mino@megazone.com>
2023-01-11 17:55:05 +0900

    docs: update description for each services
    Signed-off-by: Minho Kim <mino@megazone.com>

--- src/cloudforet/console_api_v2/conf/router_conf.py
+++ src/cloudforet/console_api_v2/conf/router_conf.py
@@ -3,289 +3,289 @@ ROUTER = [
     {
         'sub_api': 'dashboard',
         'path': 'cloudforet.console_api_v2.interface.rest.dashboard.domain_dashboard:router',
-        'prefix': '/dashboard/domain-dashboard',
+        'prefix': '/domain-dashboard',
         'tags': ['dashboard > domain-dashboard']
     },
     {
         'sub_api': 'dashboard',
         'path': 'cloudforet.console_api_v2.interface.rest.dashboard.project_dashboard:router',
-        'prefix': '/dashboard/project-dashboard',
+        'prefix': '/project-dashboard',
         'tags': ['dashboard > project-dashboard']
     },
     {
         'sub_api': 'dashboard',
         'path': 'cloudforet.console_api_v2.interface.rest.dashboard.custom_widget:router',
-        'prefix': '/dashboard/custom-widget',
+        'prefix': '/custom-widget',
         'tags': ['dashboard > custom-widget'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.domain:router',
-        'prefix': '/identity/domain',
+        'prefix': '/domain',
         'tags': ['identity > domain'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.domain_owner:router',
-        'prefix': '/identity/domain-owner',
+        'prefix': '/domain-owner',
         'tags': ['identity > domain-owner'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.endpoint:router',
-        'prefix': '/identity/endpoint',
+        'prefix': '/endpoint',
         'tags': ['identity > endpoint'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.provider:router',
-        'prefix': '/identity/provider',
+        'prefix': '/provider',
         'tags': ['identity > provider'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.service_account:router',
-        'prefix': '/identity/service-account',
+        'prefix': '/service-account',
         'tags': ['identity > service-account'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.project_group:router',
-        'prefix': '/identity/project-group',
+        'prefix': '/project-group',
         'tags': ['identity > project-group'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.project:router',
-        'prefix': '/identity/project',
+        'prefix': '/project',
         'tags': ['identity > project'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.policy:router',
-        'prefix': '/identity/policy',
+        'prefix': '/policy',
         'tags': ['identity > policy'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.role:router',
-        'prefix': '/identity/role',
+        'prefix': '/role',
         'tags': ['identity > role'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.role_binding:router',
-        'prefix': '/identity/role-biding',
+        'prefix': '/role-biding',
         'tags': ['identity > role-binding'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.user:router',
-        'prefix': '/identity/user',
+        'prefix': '/user',
         'tags': ['identity > user'],
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.api_key:router',
-        'prefix': '/identity/api-key',
+        'prefix': '/api-key',
         'tags': ['identity > api-key']
     },
     {
         'sub_api': 'identity',
         'path': 'cloudforet.console_api_v2.interface.rest.identity.token:router',
-        'prefix': '/identity/token',
+        'prefix': '/token',
         'tags': ['identity > token']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.region:router',
-        'prefix': '/inventory/region',
+        'prefix': '/region',
         'tags': ['inventory > region']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.cloud_service_type:router',
-        'prefix': '/inventory/cloud-service-type',
+        'prefix': '/cloud-service-type',
         'tags': ['inventory > cloud-service-type']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.cloud_service:router',
-        'prefix': '/inventory/cloud-service',
+        'prefix': '/cloud-service',
         'tags': ['inventory > cloud-service']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.cloud_service_tag:router',
-        'prefix': '/inventory/cloud-service-tag',
+        'prefix': '/cloud-service-tag',
         'tags': ['inventory > cloud-service-tag']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.change_history:router',
-        'prefix': '/inventory/change-history',
+        'prefix': '/change-history',
         'tags': ['inventory > change-history']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.note:router',
-        'prefix': '/inventory/note',
+        'prefix': '/note',
         'tags': ['inventory > note']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.resource_group:router',
-        'prefix': '/inventory/resource_group',
+        'prefix': '/resource_group',
         'tags': ['inventory > resource_group']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.collector:router',
-        'prefix': '/inventory/collector',
+        'prefix': '/collector',
         'tags': ['inventory > collector']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.job:router',
-        'prefix': '/inventory/job',
+        'prefix': '/job',
         'tags': ['inventory > job']
     },
     {
         'sub_api': 'inventory',
         'path': 'cloudforet.console_api_v2.interface.rest.inventory.job_task:router',
-        'prefix': '/inventory/job_task',
+        'prefix': '/job_task',
         'tags': ['inventory > job-task']
     },
     {
         'sub_api': 'repository',
         'path': 'cloudforet.console_api_v2.interface.rest.repository.repository:router',
-        'prefix': '/repository/repository',
+        'prefix': '/repository',
         'tags': ['repository > repository']
     },
     {
         'sub_api': 'repository',
         'path': 'cloudforet.console_api_v2.interface.rest.repository.plugin:router',
-        'prefix': '/repository/plugin',
+        'prefix': '/plugin',
         'tags': ['repository > plugin']
     },
     {
         'sub_api': 'repository',
         'path': 'cloudforet.console_api_v2.interface.rest.repository.schema:router',
-        'prefix': '/repository/schema',
+        'prefix': '/schema',
         'tags': ['repository > schema']
     },
     {
         'sub_api': 'repository',
         'path': 'cloudforet.console_api_v2.interface.rest.repository.policy:router',
-        'prefix': '/repository/policy',
+        'prefix': '/policy',
         'tags': ['repository > policy']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.protocol:router',
-        'prefix': '/notification/protocol',
+        'prefix': '/protocol',
         'tags': ['notification > protocol']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.quota:router',
-        'prefix': '/notification/quota',
+        'prefix': '/quota',
         'tags': ['notification > quota']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.notification_usage:router',
-        'prefix': '/notification/notification-usage',
+        'prefix': '/notification-usage',
         'tags': ['notification > notification-usage']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.project_channel:router',
-        'prefix': '/notification/project-channel',
+        'prefix': '/project-channel',
         'tags': ['notification > project-channel']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.user_channel:router',
-        'prefix': '/notification/user-channel',
+        'prefix': '/user-channel',
         'tags': ['notification > user-channel']
     },
     {
         'sub_api': 'notification',
         'path': 'cloudforet.console_api_v2.interface.rest.notification.notification:router',
-        'prefix': '/notification/notification',
+        'prefix': '/notification',
         'tags': ['notification > notification']
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.data_source:router',
-        'prefix': '/cost-analysis/data-source',
+        'prefix': '/data-source',
         'tags': ['cost-analysis > data-source'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.exchange_rate:router',
-        'prefix': '/cost-analysis/exchange-rate',
+        'prefix': '/exchange-rate',
         'tags': ['cost-analysis > exchange-rate'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.cost:router',
-        'prefix': '/cost-analysis/cost',
+        'prefix': '/cost',
         'tags': ['cost-analysis > cost'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.data_source_rule:router',
-        'prefix': '/cost-analysis/data-source-rule',
+        'prefix': '/data-source-rule',
         'tags': ['cost-analysis > data-source-rule'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.budget:router',
-        'prefix': '/cost-analysis/budget',
+        'prefix': '/budget',
         'tags': ['cost-analysis > budget'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.budget_usage:router',
-        'prefix': '/cost-analysis/budget-usage',
+        'prefix': '/budget-usage',
         'tags': ['cost-analysis > budget-usage'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.public_dashboard:router',
-        'prefix': '/cost-analysis/public-dashboard',
+        'prefix': '/public-dashboard',
         'tags': ['cost-analysis > public-dashboard'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.user_dashboard:router',
-        'prefix': '/cost-analysis/user-dashboard',
+        'prefix': '/user-dashboard',
         'tags': ['cost-analysis > user-dashboard'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.cost_query_set:router',
-        'prefix': '/cost-analysis/cost-query-set',
+        'prefix': '/cost-query-set',
         'tags': ['cost-analysis > cost-query-set'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.custom_widget:router',
-        'prefix': '/cost-analysis/custom-widget',
+        'prefix': '/custom-widget',
         'tags': ['cost-analysis > custom-widget'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.job:router',
-        'prefix': '/cost-analysis/job',
-        'tags': ['cost-analysis > job'],
+        'prefix': '/job',
+        'tags': ['/cost-analysis > job'],
     },
     {
         'sub_api': 'cost-analysis',
         'path': 'cloudforet.console_api_v2.interface.rest.cost_analysis.job_task:router',
-        'prefix': '/cost-analysis/job-task',
+        'prefix': '/job-task',
         'tags': ['cost-analysis > job-task'],
     }
 ]
