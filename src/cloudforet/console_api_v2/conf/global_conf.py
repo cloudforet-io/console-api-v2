3201a3565abee62b88c5431fa4d5d51b6b2212ab
Minho Kim <mino@megazone.com>
2023-01-16 19:52:57 +0900

    docs: modify docs description
    Signed-off-by: Minho Kim <mino@megazone.com>

--- src/cloudforet/console_api_v2/conf/global_conf.py
+++ src/cloudforet/console_api_v2/conf/global_conf.py
@@ -3,16 +3,6 @@ DESCRIPTION = """
 You need api key for authorization. 
 
 If you want to issue api key, please read this [documentation](https://cloudforet.io/docs/guides/my-page/access-with-api-cli/) first.
-
-<br>
-| Service                              | Description                                                     | OpenAPI JSON                            |
-|--------------------------------------|-----------------------------------------------------------------|-----------------------------------------|
-| [Identity](/identity/docs)           | Manage users, roles, permissions to each project easily.        | [download](/identity/openapi.json)      |
-| [Inventory](/inventory/docs)         | Discover and manage multi-cloud resources.                      | [download](/inventory/openapi.json)     |
-| [Notification](/notification/docs)   | Notify events and manage incidents automatically.               | [download](/notification/openapi.json)  |
-| [Repository](/repository/docs)       | Register and manage your deployable plugins, schema and policy. | [download](/repository/openapi.json)    |
-| [Cost-analysis](/cost-analysis/docs) | Analyze your costs easily and optimize resources.               | [download](/cost-analysis/openapi.json) |
-| [Dashboard](/dashboard/docs)         | Set and customize your dashboard and widget.                    | [download](/dashboard/openapi.json)     |
 """
 
 UVICORN_OPTIONS = {}
