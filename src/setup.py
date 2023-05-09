0492246744f921c6c8c60fcb73c798b526409fc7
G.J.LEE <glee@mz.co.kr>
2023-05-09 11:09:49 +0900

    fix: get VERSION from env var
    Signed-off-by: G.J.LEE <glee@mz.co.kr>

--- src/setup.py
+++ src/setup.py
@@ -13,15 +13,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import os
 from setuptools import setup, find_packages
-
-with open('VERSION', 'r') as f:
-    VERSION = f.read().strip()
-    f.close()
 
 setup(
     name='cloudforet-console-api-v2',
-    version=VERSION,
+    version=os.environ.get('PACKAGE_VERSION'),
     description='Cloudforet Console API v2 Service',
     long_description='',
     url='https://cloudforet.io/',
