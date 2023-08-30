109d97995cfd435dfde618e1350154c55ebb62bf
ImMin5 <mino@megazone.com>
2023-08-30 10:28:23 +0900

    feat: add description field to openapi spec (#57)
    Signed-off-by: ImMin5 <mino@megazone.com>
    (cherry picked from commit 20aef0f8f760f61b7b2c6b78ce8474739902fee7)

--- src/cloudforet/console_api_v2/model/__init__.py
+++ src/cloudforet/console_api_v2/model/__init__.py
@@ -5,12 +5,42 @@ class BaseAPIModel(BaseModel):
     @classmethod
     def meta(cls):
         return {
+            "description": cls.description(),
             "requestBody": {
+                "description": cls.request_body_description(),
                 "required": True,
                 "content": {
                     "application/json": {
-                        "schema": cls.schema()
+                        "schema": cls.schema(),
+                        "examples": {
+                            "Schema": {}
+                        },
                     }
                 }
             }
         }
+
+    @classmethod
+    def description(cls):
+        return '### Description\n' + getattr(cls.Config, 'description', '')
+
+    @classmethod
+    def request_body_description(cls):
+        table_body_format = "|{key}|{description}|{type}|{required}|\n"
+        properties = cls.schema().get('properties')
+        required_fields = cls.schema().get('required')
+
+        table_header = "\n" \
+                       "\n| Key               | Description                                                   | Type      | Required|" \
+                       "\n|-------------------|---------------------------------------------------------------|-----------|-------|\n"
+        table_body = ""
+
+        for filed, value in properties.items():
+            description = value.get('description', '')
+            required = 'True' if filed in required_fields else ''
+
+            table_body += table_body_format.format(key=filed, description=description, type=value.get('type'),
+                                                   required=required)
+
+        table_description = table_header + table_body
+        return table_description
