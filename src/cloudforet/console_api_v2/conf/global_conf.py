bc1d7320a033ca4d7b4d794b6a5050a10bc8231b
ImMin5 <mino@megazone.com>
2023-09-06 14:07:48 +0900

    feat: add swagger router (#57)
    Signed-off-by: ImMin5 <mino@megazone.com>
    (cherry picked from commit fa5f10411cc24d68fa96be2e56f8721c1b7b2803)

--- src/cloudforet/console_api_v2/conf/global_conf.py
+++ src/cloudforet/console_api_v2/conf/global_conf.py
@@ -1,6 +1,5 @@
-EXTENSION_SWAGGER_PATH = '/opt/cloudforet/openapi'
-TITLE = 'Http API for Cloudforet'
-DESCRIPTION = """
+REST_TITLE = 'Http API for Cloudforet'
+REST_DESCRIPTION = """
 You need to issue api key to use Cloudforet API.
 <br>
 Please read this user [guide](https://cloudforet.io/docs/guides/my-page/access-with-api-cli/) first.
@@ -11,9 +10,32 @@ After issuing api key, enter the api key in the [Authorize] button on the right 
 If you're interested in learning more about Cloudforet, please visit our [website](https://cloudforet.io/).
 <br>
 For a more comprehensive understanding of the Cloudforet API, please visit our [documentation](https://cloudforet-io.github.io/api-doc/).
+<br>
+<br>
+## List of Services \n
+[Home](/docs)\n
+| **Name** | **Description**  |
+|---|---|
+| [Identity](/identity/docs) | This is a Identity Service | 
+| [Inventory](/inventory/docs) | This is a Inventory Service | 
+| [Cost Analysis](/cost-analysis/docs) | This is a Board Service |
+| [Monitoring](/monitoring/docs) | This is a Monitoring Service | 
+| [Notification](/notification/docs) | This is a Notification Service | 
+| [Repository](/repository/docs) | This is a Repository Service | 
+| [Board](/board/docs) | This is a Board Service | 
+| [Config](/config/docs) | This is a Config Service | 
+| [Dashboard](/dashboard/docs) | This is a Dashboard Service | 
+| [File Manager](/file-manager/docs) | This is a File Manager Service | 
+| [Plugin](/plugin/docs) | This is a Plugin Service | 
+| [Secret](/secret/docs) | This is a Secret Service | 
+| [Statistics](/statistics/docs) | This is a Statistics Service | 
 """
 
-UVICORN_OPTIONS = {}
+OPENAPI_JSON_DIR = '/opt/cloudforet/openapi'
+
+UVICORN_OPTIONS = {
+    'factory': True
+}
 
 LOG = {
     'loggers': {
@@ -23,6 +45,7 @@ LOG = {
         }
     }
 }
+
 
 CONNECTORS = {
     'SpaceConnector': {
