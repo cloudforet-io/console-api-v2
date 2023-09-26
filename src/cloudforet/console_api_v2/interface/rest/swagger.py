708fb171265dcc201cd1d99ea7448a255bd4cb32
ImMin5 <mino@megazone.com>
2023-09-26 17:30:33 +0900

    feat: modify _get_openapi_json_data function (#57)
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/cloudforet/console_api_v2/interface/rest/swagger.py
+++ src/cloudforet/console_api_v2/interface/rest/swagger.py
@@ -1,4 +1,5 @@
 import logging
+import glob
 import json
 import os
 
@@ -7,6 +8,7 @@ from fastapi.openapi.docs import get_swagger_ui_html
 
 from spaceone.core import config
 from spaceone.core.cache import cacheable
+from spaceone.core.fastapi.api import exception_handler
 from cloudforet.console_api_v2.error.swagger import *
 
 
@@ -17,17 +19,22 @@ router = APIRouter(include_in_schema=False)
 
 @cacheable(key='openapi-json:{service}', backend='local')
 def _get_openapi_json_data(service):
-    openapi_json_dir = config.get_global('OPENAPI_JSON_DIR')
-    openapi_json_file_path = f"{openapi_json_dir}/{service.replace('-','_')}_openapi.json"
-    if not os.path.exists(openapi_json_file_path):
-        raise ERROR_SERVICE_NOT_FOUND(service=service)
-    with open(openapi_json_file_path, 'r') as f:
-        openapi_json_data = json.loads(f.read())
-    return openapi_json_data
+    openapi_json_dirs = config.get_global('OPENAPI_JSON_DIRS', [])
+
+    for openapi_json_dir in openapi_json_dirs:
+        openapi_json_files = glob.glob(os.path.join(openapi_json_dir))
+        for openapi_json_file in openapi_json_files:
+            if service.replace('-', '_') in openapi_json_file:
+                if not os.path.exists(openapi_json_file):
+                    break
+                with open(openapi_json_file, 'r') as f:
+                    return json.loads(f.read())
+    _LOGGER.error(f'[_get_openapi_json_data] openapi.json for {service} not found')
+    raise ERROR_NOT_FOUND(key=service, value='openapi.json')
 
 
 @cacheable(key='swagger-ui-html:{service}', backend='local')
-def _get_swagger_ui_html(request:Request, service):
+def _get_swagger_ui_html(request: Request, service):
     return get_swagger_ui_html(
         openapi_url=f'/{service}/openapi.json',
         title=f'{service.title().replace("-", " ")} API' + ' - Swagger UI',
@@ -37,11 +44,13 @@ def _get_swagger_ui_html(request:Request, service):
 
 
 @router.get('/{service}/docs')
+@exception_handler
 async def docs(request: Request, service: str):
     return _get_swagger_ui_html(request, service)
 
 
 @router.get('/{service}/openapi.json')
+@exception_handler
 async def openapi_json(service: str):
     return _get_openapi_json_data(service)
 
