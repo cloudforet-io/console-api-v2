6c146135d8d49ee2a00e9ff5b0194b62c69c034c
ImMin5 <mino@megazone.com>
2023-09-26 17:21:20 +0900

    feat: add inject token when init proxy service and dispatch api  (#57)
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/cloudforet/console_api_v2/service/proxy_service.py
+++ src/cloudforet/console_api_v2/service/proxy_service.py
@@ -1,4 +1,6 @@
 import logging
+from functools import wraps
+
 from spaceone.core.service import *
 from cloudforet.console_api_v2.manager.cloudforet_manager import CloudforetManager
 
@@ -10,11 +12,13 @@ class ProxyService(BaseService):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if metadata := kwargs.get('metadata', {}):
+            self.token = metadata.get('token')
         self.cf_mgr: CloudforetManager = self.locator.get_manager(CloudforetManager)
 
     @transaction
     @check_required(['grpc_method'])
-    def dispatch_api(self, params):
+    async def dispatch_api(self, params):
         grpc_method = params['grpc_method']
         del params['grpc_method']
-        return self.cf_mgr.dispatch_api(grpc_method, params)
+        return self.cf_mgr.dispatch_api(grpc_method, params, self.token)
