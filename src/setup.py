efe8ef3ccd29056aed0f48e4710b683e255193d8
ImMin5 <mino@megazone.com>
2023-12-23 19:04:38 +0900

    build: fix typo at setup.py
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/setup.py
+++ src/setup.py
@@ -27,7 +27,7 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(),
     install_requires=[
-        'spaceone-core'
+        'spaceone-core',
         'spaceone-api'
     ],
     zip_safe=False,
