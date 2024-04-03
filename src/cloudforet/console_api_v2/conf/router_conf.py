fa88db5353f4159d81fedfb1262596b4f46b6478
Youngjin Jo <youngjinjo@megazone.com>
2024-04-03 14:35:16 +0900

    feat: add kubernetes service account agent mode
    Signed-off-by: Youngjin Jo <youngjinjo@megazone.com>

--- src/cloudforet/console_api_v2/conf/router_conf.py
+++ src/cloudforet/console_api_v2/conf/router_conf.py
@@ -1,7 +1,10 @@
 ROUTER = [
     {
         "router_path": "cloudforet.console_api_v2.interface.rest.api:router",
-        "router_options": {"prefix": "/console-api/api", "tags": ["console-api > api"]},
+        "router_options": {
+            "prefix": "/console-api/api",
+            "tags": ["console-api > api"],
+        },
     },
     {
         "router_path": "cloudforet.console_api_v2.interface.rest.auth:router",
@@ -17,7 +20,9 @@ ROUTER = [
             "tags": ["console-api > extension > service-account" > "agent"],
         },
     },
-    {"router_path": "cloudforet.console_api_v2.interface.rest.swagger:router"},
+    {
+        "router_path": "cloudforet.console_api_v2.interface.rest.swagger:router",
+    },
     {
         "router_path": "cloudforet.console_api_v2.interface.rest.proxy:router",
     },
--- src/cloudforet/console_api_v2/interface/rest/extension/service_account/agent.py
+++ src/cloudforet/console_api_v2/interface/rest/extension/service_account/agent.py
@@ -1,14 +1,12 @@
 import logging
 
-import yaml
 from fastapi import Request, Depends, Response
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from spaceone.core.fastapi.api import BaseAPI, exception_handler
 
-from cloudforet.console_api_v2.service.auth_service import AuthService
-from cloudforet.console_api_v2.manager.cloudforet_manager import CloudforetManager
+from cloudforet.console_api_v2.service.agent_service import AgentService
 
 _LOGGER = logging.getLogger(__name__)
 _AUTH_SCHEME = HTTPBearer()
@@ -19,27 +17,15 @@ router = InferringRouter(include_in_schema=True)
 @cbv(router)
 class Agent(BaseAPI):
     token: HTTPAuthorizationCredentials = Depends(_AUTH_SCHEME)
-    service = "service-account"
+    service = "Agent"
 
     @router.get("/kubernetes")
     @exception_handler
     async def kubernetes(self, request: Request, service_account_id: str):
         token = self.token.credentials
-        # spaceone clinet sercret 검증
-        auth_service = AuthService()
-        auth_service.basic({"username": service_account_id, "password": token})
-        # service account id 확인
-        cloudforet_mgr = CloudforetManager()
-        cloudforet_mgr.dispatch_api(
-            "identity.ServiceAccount",
-            params={"service_account_id": service_account_id},
-            token=token,
-        )
-        # yaml 파일 혹은 response return
-        data = {"service_account_id": service_account_id}
-        data = yaml.dump(data)
 
-        return Response(content=data, media_type="application/x-yaml")
+        agent_service = AgentService()
+        params = {"service_account_id": service_account_id, "token": token}
+        spaceone_agent = agent_service.kubernetes(params)
 
-    def check_service_account(self, service_account_id):
-        pass
+        return Response(content=spaceone_agent, media_type="application/x-yaml")
